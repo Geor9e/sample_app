94a62b7032dfcdda1b818e8ebeb7a19486d7aad7
6e0r9e <george@6e0r9e.com>
2012-07-09 17:39:53 -0400

    Finish the signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 SampleApp::Application.routes.draw do
-  root to: 'static_pages#home'
+  resources :users
 
-  match '/signup', to: 'users#new'
+
+  root  :to => 'static_pages#home'
+
+  match '/signup',  to: 'users#new'
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
